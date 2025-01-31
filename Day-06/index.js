46544a8262137323d1337f30005072430ac672d7
AvinashT31 <115381340+DakhaniUsman@users.noreply.github.com>
2025-01-31 14:35:38 +0530

    JS Day 06

--- Day-06/index.js
+++ Day-06/index.js
@@ -1,3 +1,49 @@
+// Datatye in JavaSript string, number, boolean, null, undefined
+// variable types var, let and const
+// Operators    Arihhmetic, Comparison
+
+// Arihtmetic Operators
+
+// Conditional Statements if else, switch
+
+// Loops - For Loop (Forward)
+
+// Loops - For Loop (Reverse)
+
+// Loops - while loop (Forward) (Reverse)
+
+// Loops - break and continue
+
+// Undefined datatype
+
+// Functions - 1. Named Function (Traditional Function), 2. Anonymous Function (Arrow Function)
+
+var name = "Kirito"
+
+// Named Function (Traditional Function)
+function myFunction(name) {
+    console.log("Function is executed");
+    console.log("Hello 😁", name);
+
+}
+myFunction(name);
+
+// Arrow Function (anonymous Function)
+
+const newFunction = (name) => {
+    console.log("Arrow Function is executed");
+    console.log("Hello 😁", name);
+}
+
+newFunction(name);
+
+// for single prop
+
+const newMyFunction = prop => {console.log(prop)};
+newMyFunction("Hello!! Kirito");
+
+// Hoisting is not applicable in arrow function
+
 // Q. arrow function to count even and odd nubmber between range 10 to 30
 
 
